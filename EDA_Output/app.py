0cb1823f99235dd0218dfecd4db41bd11be8aa53
Prince Varghese <32154936+princevarghesedev@users.noreply.github.com>
2019-05-11 00:27:46 +0530

    Delete app.py

--- EDA_Output/app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,render_template
-app = Flask(__name__)
-
-@app.route('/index')
-def index():
-    return render_template("index_project.html")
-
-if __name__ == '__main__':
-   app.run(debug=True,use_reloader=True)
