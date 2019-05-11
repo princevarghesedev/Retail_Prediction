97773df3f2df13b5ce3b3d490524858db25b6c2b
Prince Varghese <32154936+princevarghesedev@users.noreply.github.com>
2019-05-11 19:44:22 +0530

    Update product_based_EDA.py

--- product_based_EDA.py
+++ product_based_EDA.py
@@ -27,7 +27,7 @@ df=df.loc[start:end]
 # print(df.index)
 y = df['sales'].resample('MS').sum()
 print(y)
-#
+
 # y1=y['2015-01-01':'2015-12-31']
 # y2=y['2016-01-01':'2016-12-31']
 # y3=y['2017-01-01':'2017-12-31']
@@ -37,25 +37,25 @@ print(y)
 # plt.show()
 # y3.plot(figsize=(15, 6))
 
-# y.plot(figsize=(15, 6))
-# plt.show()
-#
-# rcParams['figure.figsize'] = 18, 8
-# decomposition = sm.tsa.seasonal_decompose(y, model='additive')
-# fig = decomposition.plot()
-# plt.show()
+y.plot(figsize=(15, 6))
+plt.show()
 
-# pro_grp=df.groupby('transactionDate')['product_description'].sum().reset_index()
-# df=df.set_index('transactionDate')
-# x = df['product_description'].value_counts()
-# p_count= x.iloc[[2,5,6,8,9,10,13,14,15,16],]
-# xaxis_name= p_count.unique()
-## print(p_count)
-# y_pos = np.arange(len(p_count))
-# p_bar = plt.barh(y_pos,p_count, align='center', alpha=0.5)
-# plt.yticks(y_pos,p_count.keys(),fontsize=7,weight='light')
-# plt.xlabel('count')
-# plt.title('Item')
-# plt.show()
+rcParams['figure.figsize'] = 18, 8
+decomposition = sm.tsa.seasonal_decompose(y, model='additive')
+fig = decomposition.plot()
+plt.show()
+
+pro_grp=df.groupby('transactionDate')['product_description'].sum().reset_index()
+df=df.set_index('transactionDate')
+x = df['product_description'].value_counts()
+p_count= x.iloc[[2,5,6,8,9,10,13,14,15,16],]
+xaxis_name= p_count.unique()
+# print(p_count)
+y_pos = np.arange(len(p_count))
+p_bar = plt.barh(y_pos,p_count, align='center', alpha=0.5)
+plt.yticks(y_pos,p_count.keys(),fontsize=7,weight='light')
+plt.xlabel('count')
+plt.title('Item')
+plt.show()
 
 # print(y.head())
